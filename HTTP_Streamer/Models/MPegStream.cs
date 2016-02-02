ecc56a3f3f2fd28e0a850ca7a279d511694ebb99
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2016-02-02 22:12:53 +0000

    Adding the auto regulation, algorithm not quite working yet

--- HTTP_Streamer/Models/MPegStream.cs
+++ HTTP_Streamer/Models/MPegStream.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.IO;
 using Tools;
 using System.Configuration;
+using System.Diagnostics;
 
 namespace HTTP_Streamer.Models
 {
@@ -19,24 +20,38 @@ namespace HTTP_Streamer.Models
     {
         public string boundary { get; set; } = "myboundary";
 
+        //Frame regulation - REFACTOR!
+        private int currentDelayMs; //current delay in milliseconds
+        private int naturalFramesPerMinute; //frames per minute the web service would run without a delay
+        private Stopwatch regulatorClock; //times the speed between frames
+        private int framesPerRegulationCheck; //how many frames in a speed check
+        private int frameCount; //the number of frames in this regulation section
+        private int frameRate = 33; //to be re-set by the meta data file
+
         private string sessionKey { get; set; }
         private int cameraId { get; set; }
         private string filePath { get; set; }
-        /// <summary>
-        /// Defines the file path in which the .jpg files will merged
-        /// </summary>
-        /// <param name="filePath"></param>
-
 
         public MPegStream(int cameraId, string sessionKey)
         {
             this.sessionKey = sessionKey;
             this.cameraId = cameraId;
+            Setup();
         }
 
         public MPegStream(string filePath)
         {
             this.filePath = filePath;
+            Setup();
+        }
+
+        private void Setup()
+        {
+            naturalFramesPerMinute = 1500; //start somewhere, move to config
+            currentDelayMs = 49;
+            frameCount = 0;
+            regulatorClock = new Stopwatch();
+            framesPerRegulationCheck = 100;
         }
 
         public async Task WriteToStream(Stream outputStream, HttpContent content, TransportContext context)
@@ -53,7 +68,8 @@ namespace HTTP_Streamer.Models
                     await outputStream.WriteAsync(header, 0, header.Length); //write the header                  
                     await fileInfo.OpenRead().CopyToAsync(outputStream); //write the JPEG bytes 
                     await outputStream.WriteAsync(crlf, 0, crlf.Length); //write the new line 
-                    await Task.Delay(new TimeSpan(0,0,0,0,49)); //a ms delay to regulate the speed
+                    await Task.Delay(Delay()); //a ms delay to regulate the speed
+                    await RegulateFramerate(); //set the delay
 
                  }//each file
 
@@ -119,6 +135,50 @@ namespace HTTP_Streamer.Models
             return ret;
 
         }//ImageFiles
+
+        public TimeSpan Delay()
+        {
+            return new TimeSpan(0, 0, 0, 0, currentDelayMs);
+        }
+
+        public async Task RegulateFramerate()
+        {
+            await Task.Run(() =>
+            {              
+                //start the stopwatch on the initial image creation
+                if(!regulatorClock.IsRunning)
+                {
+                    regulatorClock.Start();
+                }
+                else
+                {
+                    frameCount++;
+
+                    if(frameCount % framesPerRegulationCheck == 0)
+                    {
+                        //section is full, calculate the natural speed
+                        regulatorClock.Stop();
+                        double totalDelayMs = currentDelayMs * frameCount; 
+                        double totalTransmissionMs = regulatorClock.Elapsed.TotalMilliseconds;
+
+                        double naturalTransmission = totalTransmissionMs - totalDelayMs;
+                        double naturalFramerate = naturalTransmission / framesPerRegulationCheck;
+
+
+
+
+                        //work out what the delay should be
+                        double newDelay = (1 / naturalFramerate) * 1000;
+                       // currentDelayMs = newDelay 
+
+                        //reset everything
+                        frameCount = 0;
+                        regulatorClock.Restart();
+                    }
+                }
+
+            });
+        }//RegulateFramerate
 
     }
 }
