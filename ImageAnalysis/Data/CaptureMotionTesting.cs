2ced4e56efe7450bd4f180872be45e3f43742bd3
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2016-02-28 10:22:33 +0000

    including CaptureMotionTesting class

---
+++ ImageAnalysis/Data/CaptureMotionTesting.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageAnalysisDAL;
+using System.Configuration;
+using System.Data;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace ImageAnalysis.Data
+{
+    public class CaptureListTesting : CaptureList, ICaptureList
+    {
+        public new List<CaptureMotionTesting> list { get; set; }
+
+        /// <summary>
+        /// Populates metadata about each capture session available
+        /// allData will load additional metadata, like when movement occurs
+        /// </summary>
+        /// <param name="allData"></param>
+        public new void PopulateAllCaptures(bool allData)
+        {
+            list = new List<CaptureMotionTesting>();
+            ConnectionStringSettingsCollection connections = ConfigurationManager.ConnectionStrings;
+            captureInfo = new ImageAnalysisDAL.CaptureDb(connections["AZURE"].ConnectionString);
+            DataTable dt = captureInfo.ReturnAllCaptures();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                CaptureMotionTesting capture = new CaptureMotionTesting();
+                capture.captureId = dr.Field<String>("captureId");
+                capture.capturedOn = dr.Field<DateTime?>("capturedOn");
+                list.Add(capture);
+            }
+
+            if (allData) { PopulateMovement(); }
+
+        }//PopulateAllCaptures
+    }
+
+    /// <summary>
+    /// Used to hold testing data about a single Capture object
+    /// </summary>
+    public class CaptureMotionTesting : Capture, ICapture
+    {
+        public CaptureMotionTesting() {  }
+
+        public DateTime startTime { get; set; }
+
+        public DateTime endTime { get; set; }
+
+        public String detectionMethod { get; set; }
+
+        /// <summary>
+        /// What frame numbers have been identified as having movement
+        /// </summary>
+        public List<Int32> detectedMovmentFrames { get; set; }
+
+
+        /// <summary>
+        /// Returns information about the CaptureMotionTesting 
+        /// </summary>
+        /// <returns></returns>
+        public XmlDocument MotionTestingXml()
+        {
+
+ 
+           //serialise testing object XML and return
+            XmlDocument doc = new XmlDocument();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                XmlSerializer x = new XmlSerializer(typeof(CaptureMotionTesting));
+                x.Serialize(stream, this);
+                stream.Seek(0, System.IO.SeekOrigin.Begin); //without this there is a 'missing' root element error
+                doc.Load(stream);
+            }
+
+            return doc;
+
+        }//DetectedMovementXml 
+
+    }
+}
---
+++ ImageAnalysis/MotionSensor/MotionSensorSettings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageAnalysis.MotionSensor
+{
+    /// <summary>
+    /// Various settings that are applied to the motion detector during testing
+    /// </summary>
+    public class MotionSensorSettings
+    {
+        /// <summary>
+        /// Whether the motion detector will analyse frames asyncrohously
+        /// </summary>
+        public bool asynchronous { get; set; }
+
+        /// <summary>
+        /// The number of frames to skip during image analysis of motion detection
+        /// Varaible is always used, defaults to 0
+        /// </summary>
+        public int framesToSkip { get { return framesToSkip; } set { if (value >= 0) { this.framesToSkip = value; }; } }
+
+        /// <summary>
+        /// The number of pixels skipped horizontally when scanning an image
+        /// Variable is always used, defaults to 0
+        /// </summary>
+        public int horizontalPixelsToSkip { get { return horizontalPixelsToSkip; } set { if (value >= 0) { this.horizontalPixelsToSkip = value; }; } }
+
+        /// <summary>
+        /// The number of pixels skipped vertically when scanning an image
+        /// Variable is always used, defaults to 0
+        /// </summary>
+        public int verticalPixelsToSkip { get { return verticalPixelsToSkip; } set { if (value >= 0) { this.verticalPixelsToSkip = value; }; } }
+
+        /// <summary>
+        /// Multiplier used when determining whether there is motion, or not. Defaults to 1. Less than 1 increases
+        /// sensitivity, great than 1 decreases sensitivity
+        /// </summary>
+        public double sensitivity { get; set; }
+
+        /// <summary>
+        /// The number of pixels to search horizontally. Defaults to the image width if not set
+        /// </summary>
+        public int searchWidth { get; set; }
+
+        /// <summary>
+        /// The number of pixels to search vertically. Defaults to the image height if not set
+        /// </summary>
+        public int searchHeight { get; set; }
+
+        /// <summary>
+        /// Whether a compare action should try and share data with the next compare action
+        /// only applicable when running syncrohously
+        /// </summary>
+        public bool linkCompare { get; set; }
+
+        public MotionSensorSettings() { framesToSkip = 0; horizontalPixelsToSkip = 0; verticalPixelsToSkip = 0; }
+
+    }
+}
