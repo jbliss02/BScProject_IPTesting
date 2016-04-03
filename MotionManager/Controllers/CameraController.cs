24111cacadd5f3882fed822f3600bba24a9152d3
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2016-04-03 13:22:53 +0100

    Creation of  MotionManager project, a set of WEBAPIs which will be used by the presentation layer to access Motion Sensor logic

---
+++ MotionManager/Controllers/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace MotionManager.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/[controller]")]
+    public class CameraController : Controller
+    {
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "value";
+        }
+
+
+    }
+}
---
+++ MotionManager/Controllers/DetectionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MotionManager.Controllers
+{
+    [Route("api/[controller]")]
+    public class DetectionController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
