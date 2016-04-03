e63c7e02f4d95cf31eae3369c53cd1d636d9e74a
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2016-04-03 13:45:16 +0100

    Removed MotionManager

---
+++ ImageAnalysis/Camera/CameraFinder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ImageAnalysis.Streams;
+using ImageAnalysis.Images.Jpeg;
+
+namespace ImageAnalysis.Camera
+{
+    /// <summary>
+    /// Tries to connect to a camera at the specified IP location
+    /// </summary>
+    public class CameraFinder
+    {
+        IPAddress ipAddress;
+
+        public bool ConnectSuccess { get; set; }
+        public byte[] CameraImage { get; set; }
+        private string cameraUrl { get; set; }
+
+        public CameraFinder(string ipAddress)
+        {
+           // ipAddress = ip;
+            cameraUrl = ipAddress + @"/axis-cgi/mjpg/video.cgi";
+        }
+
+        public void GetImage()
+        {
+
+                ImageExtractor imageExtractor = new ImageExtractor(cameraUrl, "root", "root");
+                imageExtractor.asyncrohous = false; //need to wait for a success or fail
+                imageExtractor.imageCreated += new ImageExtractor.ImageCreatedEvent(ImageExtracted);
+                imageExtractor.Run(true);
+
+        }
+
+        private void ImageExtracted(ByteWrapper img, EventArgs e)
+        {
+            CameraImage = img.bytes;
+            ConnectSuccess = true;
+        }
+
+    }
+}
