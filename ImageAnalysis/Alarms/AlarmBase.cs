4e04172e7ef2c7cc5571db10259bb5040350a09b
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2016-05-02 14:50:37 +0100

    changes to Alarm so old extracted motion images are not taken into account when seeing if enough images have been created (1 second or less difference)

--- ImageAnalysis/Alarms/AlarmBase.cs
+++ ImageAnalysis/Alarms/AlarmBase.cs
@@ -21,6 +21,8 @@ namespace ImageAnalysis.Alarms
         private DateTime? lastAlarm; //when the last alarm was sounded
         private int secondsBetweenAlarms; //what delay should be added so continuous alarms are not raised
         private int minimumMovementsForAlarm; //only sounds the alarm after this many movements
+        private List<DateTime> lastMovementReceived; //when the movements were received (so old images don't fire the alarm)
+        object lastMovementReceivedLock = new object(); //locks the lastMovementReceived list
 
         public AlarmBase()
         {
@@ -28,15 +30,25 @@ namespace ImageAnalysis.Alarms
             secondsBetweenAlarms = ConfigurationManager.AppSettings["secondsBetweenAlarms"].ToString().StringToInt();
             minimumMovementsForAlarm = ConfigurationManager.AppSettings["minimumMovementsForAlarm"].ToString().StringToInt();
             lastAlarm = null;
+            lastMovementReceived = new List<DateTime>();
         }
 
         public void ImageExtracted(String imagePath, EventArgs e)
         {
             lock(objLock){images.Add(imagePath);}
 
+            lock (lastMovementReceivedLock)
+            {
+                if(lastMovementReceived.Count > minimumMovementsForAlarm) {
+                    lastMovementReceived.RemoveAt(0);
+                }
+                lastMovementReceived.Add(DateTime.Now);
+            }
+
             if (lastAlarm == null || (DateTime.Now - lastAlarm.Value).Seconds > secondsBetweenAlarms)
             {
-                if(images.Count >= minimumMovementsForAlarm)
+                
+                if(AboveImageCount())
                 {
                     lastAlarm = DateTime.Now;
                     RaiseAlarm();
@@ -45,6 +57,35 @@ namespace ImageAnalysis.Alarms
             }
 
         }//ImageExtracted
+
+        /// <summary>
+        /// whether enough images have been received to raise an alarm
+        /// based on the minimumMovementsForAlarm variable
+        /// </summary>
+        /// <returns></returns>
+        private bool AboveImageCount()
+        {
+            if(images.Count >= minimumMovementsForAlarm && lastMovementReceived.Count >= minimumMovementsForAlarm)
+            {
+                if(lastMovementReceived.Count == 2)
+                {
+                    if((lastMovementReceived[1] - lastMovementReceived[0]).Seconds <= 1)
+                    {
+                        return true;
+                    }
+                }
+                else if(lastMovementReceived.Count > 2)
+                {
+                    if ((lastMovementReceived[1] - lastMovementReceived[0]).Seconds <= 1 && (lastMovementReceived[2] - lastMovementReceived[1]).Seconds <= 1)
+                    {
+                        return true;
+                    }
+                }
+
+            }
+
+            return false;
+        }
 
         public abstract void RaiseAlarm();
     }
